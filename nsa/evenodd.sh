951ed5845c8d86089e174c9281e54fa9ca668e41
vyshnav <vyshnavmohan20@gmail.com>
2024-02-22 10:57:41 +0530

    22/2/24'

--- nsa/evenodd.sh
+++ nsa/evenodd.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 read -p"Enter a number: " n
-if [ $(( n % 2 )) -eq 0 ]
+if [ $(( $n % 2 )) -eq 0 ]
 then echo "Even number"
 else
     echo "Odd number"
---
+++ nsa/sum.sh
@@ -0,0 +1,3 @@
+read -p "Enter 2 numbers: " a b
+sum=`expr $a + $b`
+echo $sum
